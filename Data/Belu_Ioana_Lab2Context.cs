0fe0aceb55af88af132588890bc03df256f0e15b
asus <asus@DESKTOP-T1UG3BD>
2022-10-20 16:49:08 +0300

    Add project files.

---
+++ Data/Belu_Ioana_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Belu_Ioana_Lab2.Models;
+
+namespace Belu_Ioana_Lab2.Data
+{
+    public class Belu_Ioana_Lab2Context : DbContext
+    {
+        public Belu_Ioana_Lab2Context (DbContextOptions<Belu_Ioana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Belu_Ioana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Belu_Ioana_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
