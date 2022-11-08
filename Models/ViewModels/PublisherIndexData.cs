66b7113f3d375c84b7642dbb0d0b99042f6e0f3b
Ioana Belu <belu.ioana@yahoo.com>
2022-11-08 17:07:25 +0200

    Laborator4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Belu_Ioana_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
