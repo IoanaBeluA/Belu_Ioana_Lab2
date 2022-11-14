2ba4152f34d2daff10efd9d26bd534ae7c7f5d39
Ioana Belu <belu.ioana@yahoo.com>
2022-11-14 22:52:52 +0200

    Laborator5

--- Data/Belu_Ioana_Lab2Context.cs
+++ Data/Belu_Ioana_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Belu_Ioana_Lab2.Data
         public DbSet<Belu_Ioana_Lab2.Models.Author> Author { get; set; }
 
         public DbSet<Belu_Ioana_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Belu_Ioana_Lab2.Models.Member> Member { get; set; }
+
+        public DbSet<Belu_Ioana_Lab2.Models.Borrowing> Borrowing { get; set; }
     }
 }
