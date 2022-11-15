abd7e97cf3fc8ec08d135bd6915176e3ff9ba277
Ioana Belu <belu.ioana@yahoo.com>
2022-11-15 16:46:32 +0200

    lab5.1

--- Migrations/20221115142048_member1.Designer.cs
+++ Migrations/20221115142048_member1.Designer.cs
@@ -4,6 +4,7 @@ using Belu_Ioana_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Belu_Ioana_Lab2.Migrations
 {
     [DbContext(typeof(Belu_Ioana_Lab2Context))]
-    partial class Belu_Ioana_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221115142048_member1")]
+    partial class member1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/LibraryIdentity/20221115142154_identity1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Belu_Ioana_Lab2.Migrations.LibraryIdentity
+{
+    public partial class identity1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
